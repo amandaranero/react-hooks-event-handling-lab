bc5fff5bdafdb79613e6de17085a70f251cb5f90
amanda ranero <amandaranero@gmail.com>
2023-03-14 13:02:41 -0400

    could review input

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
-// Code Keypad Component Here
+import React from "react"
 
 function Keypad (){
+    const enterPassword = ()=>{
+        console.log("Entering Password")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={enterPassword}/>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -11,3 +11,18 @@ ReactDOM.render(
   </div>,
   document.getElementById('root')
 );
+
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+// import "./index.css";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
